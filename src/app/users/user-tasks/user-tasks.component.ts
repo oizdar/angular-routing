d5fdb49dac535fcc6d27882abf6a6e8343ac0e73
Radosław Malinowski <malinowski.rad@gmail.com>
2025-09-15 11:03:14 +0200

    alternative way to get param from path - using subscribers

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, input } from '@angular/core';
 import { UsersService } from "../users.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,11 +8,28 @@ import { UsersService } from "../users.service";
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
+  userName = '';
   userId = input.required<string>();
 
-  constructor(private usersService: UsersService) {
+  constructor(
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute
+  ) {
   }
 
-  userName = computed(() => this.usersService.users.find(user => user.id === this.userId())?.name)
+  // userName = computed(
+  //   () => this.usersService.users.find(
+  //     user => user.id === this.userId()
+  //   )?.name
+  // );
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe({
+      next: paramMap =>
+        this.userName = this.usersService.users.find(
+          user => user.id === paramMap.get('userId')
+        )?.name || ''
+    })
+  }
 }
