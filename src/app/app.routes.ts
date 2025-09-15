1c21b67cbf44b58a02e7b966c978c806aa345b32
Radosław Malinowski <malinowski.rad@gmail.com>
2025-09-15 11:43:20 +0200

    refactor routes - separate files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
-import { TasksComponent } from "./tasks/tasks.component";
 import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { usersRoutes } from "./users/users.routes";
 
 export const routes: Routes = [
   {
@@ -13,16 +12,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      }
-    ]
+    children: usersRoutes
   },
   {
     path: '**',
