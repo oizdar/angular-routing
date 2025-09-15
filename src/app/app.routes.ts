ef64e337106ab417496a232f80b955fe8814b786
Radosław Malinowski <malinowski.rad@gmail.com>
2025-09-15 10:51:59 +0200

    users task component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { TasksComponent } from "./tasks/tasks.component";
 import { Routes } from "@angular/router";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 
 export const routes: Routes = [
   {
-    path: 'tasks',
-    component: TasksComponent,
-  }
+    path: '',
+    component: NoTaskComponent,
+  },
+  {
+    path: 'users/:userId/tasks',
+    component: UserTasksComponent,
+  },
 ];
