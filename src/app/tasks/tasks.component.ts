5a2debc7e92d10d480daaaef0d2b48eb91908d55
Radosław Malinowski <malinowski.rad@gmail.com>
2025-09-17 07:51:43 +0200

    resolving route tasks using resolver with query param

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,13 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { ResolveFn, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
+import { TasksService } from './tasks.service';
 import { Task } from './task/task.model';
-import { TasksService } from "./tasks.service";
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tasks',
@@ -13,23 +17,28 @@ import { RouterLink } from "@angular/router";
   imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
+  userTasks = input.required<Task[]>();
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>('desc');
-  userTasks = computed(() =>
-    this.tasksService
-      .allTasks()
-      .filter(task => task.userId === this.userId())
-      .sort((a, b) => {
-        if(this.order() === 'desc') {
-          return a.id > b.id ? -1 : 1;
-        } else {
-          return a.id < b.id ? -1 : 1;
-        }
-      })
-  );
-
-  constructor(
-    private tasksService: TasksService
-  ) {
-  }
+  order = input<'asc' | 'desc' | undefined>();
 }
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
