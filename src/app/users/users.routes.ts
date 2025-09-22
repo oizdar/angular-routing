75e959e10b562514171ac9cbc81eee17fd5d862a
Radosław Malinowski <malinowski.rad@gmail.com>
2025-09-22 07:39:39 +0200

    route based lazy-loading - remember to doesn't have other pats imported in same place from lazy loaded component

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,7 +1,30 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 
-import { resolveUserTasks } from '../tasks/tasks.component';
 import { canLeaveEditPage, NewTaskComponent } from '../tasks/new-task/new-task.component';
+import { TasksService } from "../tasks/tasks.service";
+import { inject } from "@angular/core";
+import { Task } from "../tasks/task/task.model";
+
+const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
 
 export const usersRoutes: Routes = [
   {
